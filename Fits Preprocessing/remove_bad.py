59a9d08187935f7607f5a1215e94deebeeac7679
dkapellusch <dkapellusch@gmail.com>
2015-11-20 12:39:05 -0700

    changed to work with new json format

--- Fits Preprocessing/remove_bad.py
+++ Fits Preprocessing/remove_bad.py
@@ -5,11 +5,11 @@ import sys,copy
 def clean_dic(dic):
     try:
         good_files = []
-        for image in dic["SCIENCE"]:  #Search passed dictionary. Note: this expects the dictionary to be in the same from as the metadata.json file
-            if dic["SCIENCE"][image]["AOLOOPST"] == "CLOSED":
+        for image in dic:  #Search dictionary built by my other script
+            if dic[image]["AOLOOPST"] == "CLOSED":
                 good_files.append(image)   #store names of good files
         return(good_files)   #return those names
     except Exception as e:
         print("ERROR: "+str(e) +" is not a key in the passed dictionary")
 
-# print(clean_dic({"SCIENCE":{"FILE":{"AOLOOPST":"CLOSED"},"FILE2":{"AOLOOPST":"OPEN"}}}))
+# print(clean_dic({"FILE":{"AOLOOPST":"CLOSED"},"FILE2":{"AOLOOPST":"OPEN"}}))
