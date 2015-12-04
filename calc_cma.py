4135430edc5ad11895d25e646f6f3599a8fbc4a2
Alby Chaj <albertachaj@email.arizona.edu>
2015-12-04 03:12:53 -0700

    added timestamp mappings

--- calc_cma.py
+++ calc_cma.py
@@ -1,21 +1,18 @@
 import numpy as np
 
 def calc_cma(filename, size):
-"""
-This function calculates the centered moving average (CMA) of the norm values
-for the image data set's top and bottom amplifiers
+    """
+    This function calculates the centered moving average (CMA) of the norm values
+    for the image data set's top and bottom amplifiers
 
-Parameters
-    filename (str) -- name of dat file containing list of FITS filenames and 
-        the norm values for the image's top and bottom amplifiers
-    size (int) -- desired subset size (between 10-20)
+    Parameters
+        filename (str) -- name of dat file containing list of FITS filenames and 
+            the norm values for the image's top and bottom amplifiers
+        size (int) -- desired subset size (between 10-20)
 
-Returns
-    dict
-"""
-    timestamps = []
-    bottom_norms = []
-    top_norms = []
+    Returns
+        dict
+    """
 
     with open(filename) as dat_file:
         for line in dat_file:
@@ -26,16 +23,23 @@ Returns
 
     # Initial smoothing
     window = np.ones(int(size)) / float(size)
-    bottom_norm_avgs = np.convolve(bottom_norms, window, 'valid')
-    top_norm_avgs = np.convolve(top_norms, window, 'valid')
+    bottom_norm_avgs = np.convolve(bottom_norms, window, 'same')
+    top_norm_avgs = np.convolve(top_norms, window, 'same')
 
     # If the size is even, then we need to smooth the smoothed values
     if size % 2 == 0:
         window = np.ones(2) / float(2)
-        bottom_norm_avgs = np.convolve(bottom_norm_avgs, window, 'valid')
-        top_norm_avgs = np.convolve(top_norm_avgs, window, 'valid')
+        bottom_norm_avgs = np.convolve(bottom_norm_avgs, window, 'same')
+        top_norm_avgs = np.convolve(top_norm_avgs, window, 'same')
+
+    bottom_norm_mappings = {}
+    top_norm_mappings = {}
+
+    for i in range(len(timestamps)):
+        bottom_norm_mappings[timestamps[i]] = bottom_norm_avgs[i]
+        top_norm_mappings[timestamps[i]] = top_norm_avgs[i]
 
     return {
-        "smoothed_btm_norms": bottom_norm_avgs, 
-        "smoothed_top_norms": top_norm_avgs,
+        "smoothed_btm_norms": bottom_norm_mappings, 
+        "smoothed_top_norms": top_norm_mappings,
     }
