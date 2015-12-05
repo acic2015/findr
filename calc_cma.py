808de3ed5dd2bbe2533b68682db25f32cbbafdd0
franka1 <franka1@email.arizona.edu>
2015-12-04 22:30:17 -0700

    Finished calc_cma version 1

--- calc_cma.py
+++ calc_cma.py
@@ -10,17 +10,14 @@ def calc_cma(filename, size):
     for the image data set's top and bottom amplifiers
 
     Parameters
-<<<<<<< HEAD
         filename (str) -- name of dat file containing list of FITS filenames and
-=======
-        filename (str) -- name of dat file containing list of FITS filenames and 
->>>>>>> 491ed825bb9e990f744ea9833fb1b71132f5ad59
             the norm values for the image's top and bottom amplifiers
         size (int) -- desired subset size (between 10-20)
 
     Returns
         dict
     """
+    image_names = []
     timestamps = []
     bottom_norms = []
     top_norms = []
@@ -28,6 +25,7 @@ def calc_cma(filename, size):
     with open(filename) as dat_file:
         for line in dat_file:
             img_norm_data = line.split()
+            image_names.append(img_norm_data[0])
             timestamps.append(img_norm_data[0][4:20])
             bottom_norms.append(float(img_norm_data[1]))
             top_norms.append(float(img_norm_data[2]))
@@ -46,33 +44,42 @@ def calc_cma(filename, size):
     bottom_norm_mappings = {}
     top_norm_mappings = {}
 
-    for i in range(len(timestamps)):
-        bottom_norm_mappings[timestamps[i]] = bottom_norm_avgs[i]
-        top_norm_mappings[timestamps[i]] = top_norm_avgs[i]
+    result = []
 
-    return {
-        "smoothed_btm_norms": bottom_norm_mappings,
-        "smoothed_top_norms": top_norm_mappings
-    }
+    for i in range(len(image_names)):
+        result.append({
+            "sci_filename": image_names[i],
+            "timestamp": timestamps[i],
+            "smoothed_btm_norm": bottom_norm_avgs[i],
+            "smoothed_top_norm": bottom_norm_avgs[i]
+        })
+        #bottom_norm_mappings[image_names[i]] = bottom_norm_avgs[i]
+        #top_norm_mappings[image_names[i]] = top_norm_avgs[i]
+
+    #return {
+    #    "smoothed_btm_norms": bottom_norm_mappings,
+    #    "smoothed_top_norms": top_norm_mappings
+    #}
+    return result
 
 
-def shift_cma(norm_filename, cma_object):
+def shift_cma(norm_filename, smoothed_sciences):
 
     dark_filenames = []
-    timestamps = []
-    bottom_norms = []
-    top_norms = []
+    dark_timestamps = []
+    dark_bottom_norms = []
+    dark_top_norms = []
 
     with open(norm_filename) as dark_norm_file:
         for line in dark_norm_file:
             dark_image_data = line.split()
             dark_filenames.append(dark_image_data[0])
-            timestamps.append(dark_image_data[0][4:20])
-            bottom_norms.append(float(dark_image_data[1]))
-            top_norms.append(float(dark_image_data[2]))
+            dark_timestamps.append(dark_image_data[0][4:20])
+            dark_bottom_norms.append(float(dark_image_data[1]))
+            dark_top_norms.append(float(dark_image_data[2]))
 
-    dark_timestamps = []
-    for t in timestamps:
+    dark_time_in_seconds = []
+    for t in dark_timestamps:
         year = t[0:4]
         month = t[4:6]
         day = t[6:8]
@@ -83,49 +90,62 @@ def shift_cma(norm_filename, cma_object):
 
         dark_time = dt.datetime(year, month, day, hours, minutes, seconds)
         dark_time = time.mktime(dark_time.timetuple()) + partial_seconds*(10**(-6))
-        # dark_time is now in seconds since epoch
-        dark_timestamps.append(dark_time)
 
-    for t in cma_object.timestamps:
-        year = t[0:4]
-        month = t[4:6]
-        day = t[6:8]
-        hours = t[8:10]
-        minutes = t[10:12]
-        seconds = t[12:14]
-        partial_seconds = t[14:20]
+        # dark_time is now in seconds since epoch
+        dark_time_in_seconds.append(dark_time)
+
+    sci_time_in_seconds = []
+    for j in range(len(smoothed_sciences)):
+        year = smoothed_sciences[j].timestamp[0:4]
+        month = smoothed_sciences[j].timestamp[4:6]
+        day = smoothed_sciences[j].timestamp[6:8]
+        hours = smoothed_sciences[j].timestamp[8:10]
+        minutes = smoothed_sciences[j].timestamp[10:12]
+        seconds = smoothed_sciences[j].timestamp[12:14]
+        partial_seconds = smoothed_sciences[j].timestamp[14:20]
 
         sci_time = dt.datetime(year, month, day, hours, minutes, seconds)
         sci_time = time.mktime(sci_time.timetuple()) + partial_seconds*(10**(-6))
+
         # sci_time is now in seconds since epoch
-        sci_timestamps.append(sci_time)
+        sci_time_in_seconds.append(sci_time)
 
-    result = []
+    # calculate deltas
+    deltas = []
+    j = 0
+    for i in range(len(dark_time_in_seconds)):
+        found = 0
+        while found == 0 & j < len(sci_time_in_seconds):
+            if abs(sci_time_in_seconds[j] - dark_time_in_seconds[i]) < abs(sci_time_in_seconds[j + 1] - dark_time_in_seconds[i]):
+                found = 1
+            j += 1
+        deltas.append({
+            "bottom": smoothed_sciences[j].smoothed_bottom_norm - dark_bottom_norms[i],
+            "top": smoothed_sciences[j].smoothed_top_norm - dark_top_norms[i]
+        })
 
-    #calculate deltas
-    for i in range(dark_timestamps):
-        sciences = []
+    # shift smoothed norms
+    result = {}
+    j = 0
+    for i in range(len(dark_time_in_seconds)):
+        found = 0
+        while found == 0 & i < len(dark_time_in_seconds):
+            if abs(dark_time_in_seconds[i] - sci_time_in_seconds[j]) < abs(dark_time_in_seconds[i+1] - sci_time_in_seconds[j]):
+                result[smoothed_sciences[j].sci_filename] = {
+                    "bottom_norm": smoothed_sciences[j].smoothed_btm_norm - deltas[i].bottom,
+                    "top_norm": smoothed_sciences[j].smoothed_top_norm - deltas[i].top
+                }
+                j += 1
+            else:
+                found = 1
 
-        if i == 0:
-            closest = 0
-            for j in sci_timestamps:
-                if abs(dark_timestamps[i] - sci_timestamps[j]) < abs(dark_timestamps[i+1] - sci_timestamps[j]):
-                    sciences.append({
-                        "filename" : cma_object[j].filename,
-                        "" : cma_object.smoothed_btm_norm
-                    })
-        elif i == len(dark_timestamps):
-
-        else:
-            for sci in sci_timestamps:
-                if:
-
-    # shift them later
-    dark_object = {
-        "dark" : "",
-        "sciences" : sciences
-    }
-    result.append(dark_object)
+        # special case for the last dark and the last few sciences
+        while j <= len(sci_time_in_seconds):
+            result[smoothed_sciences[j]].sci_filename = {
+                "bottom_norm": smoothed_sciences[j].smoothed_btm_norm - deltas[i].bottom,
+                "top_norm": smoothed_sciences[j].smoothed_top_norm - deltas[i].top
+            }
+            j += 1
 
     return result
 
